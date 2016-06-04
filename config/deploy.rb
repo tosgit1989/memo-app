e9d38f6fde9e54a8ddbf5dd39fe9261ddad3fe1c
tosgit1989 <purannaa1989@gmail.com>
2016-06-04 14:14:36 +0900

    from var/www/... to var/www/rails/...

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :branch, 'master'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/var/www/memo-app'
+set :deploy_to, '/var/www/rails/memo-app'
 
 # Default value for :scm is :git
 # set :scm, :git
